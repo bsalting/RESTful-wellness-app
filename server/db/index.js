2fbee87919ec170ce7357be3ab65f6fda91ea190
Beejay Salting <beejay.salting@gmail.com>
2022-11-19 08:08:31 -0500

    baseline

--- server/db/index.js
+++ server/db/index.js
@@ -1,24 +1,18 @@
 const conn = require('./conn');
 const User = require('./User');
 
-const syncAndSeed = async()=> {
+const syncAndSeed = async () => {
   await conn.sync({ force: true });
-  const [moe, lucy, larry, ethyl] = await Promise.all([
-    User.create({ username: 'moe', password: '123'}),
-    User.create({ username: 'lucy', password: '123' }),
-    User.create({ username: 'larry', password: '123' }),
-    User.create({ username: 'ethyl', password: '123' }),
+  const [bos] = await Promise.all([
+    User.create({ username: 'bos', password: '123' }),
   ]);
 
   return {
     users: {
-      moe,
-      lucy,
-      larry
-    }
+      bos,
+    },
   };
 };
-
 
 module.exports = {
   syncAndSeed,
