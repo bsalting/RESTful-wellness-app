85918916d9d8e745936f24407be524d769e6a7cf
Beejay Salting <beejay.salting@gmail.com>
2022-11-20 16:43:24 -0500

    nav, home styled

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../store';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 const Home = () => {
   const { auth } = useSelector((state) => state);
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <div id="div-nav">
       <div>
         Peace, {auth.username}!
         <Button onClick={() => dispatch(logout())}>Logout</Button>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function Nav() {
   return (
     <nav>
-      <span className="logo-span">Quiet Time</span>
+      <span className="logo-span">
+        <Typography
+          variant="h4"
+          sx={{
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'Lucida Grande',
+            fontWeight: 700,
+            color: 'slate gray',
+            textDecoration: 'none',
+          }}
+        >
+          RESTFul
+        </Typography>
+      </span>
       <span className="link-span">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/meditation">Meditate</NavLink>
-        <NavLink to="/park">Meander</NavLink>
-        <NavLink to="/journal">Gratitude</NavLink>
+        <Link underline="hover" key={'Home'} href={'/'}>
+          <Button>
+            <Typography> Home </Typography>
+          </Button>
+        </Link>
+        <Link underline="hover" key={'Meditate'} href={'#/meditation'}>
+          <Button>
+            <Typography> Meditate </Typography>
+          </Button>
+        </Link>
+        <Link underline="hover" key={'Meander'} href={'#/park'}>
+          <Button>
+            <Typography> Meander </Typography>
+          </Button>
+        </Link>
+        <Link underline="hover" key={'Gratitude'} href={'#/journal'}>
+          <Button>
+            <Typography> Gratitude </Typography>
+          </Button>
+        </Link>
       </span>
     </nav>
   );
