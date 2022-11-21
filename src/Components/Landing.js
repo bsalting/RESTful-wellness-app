7e6d7359fdbd8d830ecaf8328efd2d363502f4fc
Beejay Salting <beejay.salting@gmail.com>
2022-11-21 11:52:07 -0500

    footer

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,19 +1,58 @@
 import React from 'react';
-import { Container, Box } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 
 const Landing = () => {
   return (
-    <Container>
-      <Box>
-        <div>
-          <div>Hello! Welcome to RESTful.</div>
-          <img
-            src="https://media.istockphoto.com/id/1300620744/vector/balancing-zen-stone-vector-icon-illustration.jpg?s=612x612&w=0&k=20&c=hnnXJ9Q2EZXf45t1WLKAGHaohGGKkROwLYZZYKDUqnQ="
-            width="200"
-          ></img>
-        </div>
-      </Box>
-    </Container>
+    <div>
+      <Container>
+        <Grid container>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <Grid item xs={12} sm={12} align="center">
+            <div></div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <div>Hello this</div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <div>Hello this it a dasldklk </div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <div>Whether you are blah blah blah.</div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <div>Hello! Welcome to</div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <div>Hello!</div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <div>Hello!</div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <div>Welcome to RESTful.</div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <div>Whether you are blah blah blah.</div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <div>Hello! Welcome to</div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <div>Hello!</div>
+          </Grid>
+          <Grid item xs={12} sm={12} align="center">
+            <img src="/static/logo.png" width="400"></img>
+          </Grid>
+        </Grid>
+      </Container>
+      <div id="horizon">
+        <p>About US Let's connect </p>
+      </div>
+    </div>
   );
 };
 
